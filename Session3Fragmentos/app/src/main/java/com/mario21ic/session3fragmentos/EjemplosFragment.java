e17fdb96ca6bac4f2d5e832dbb75d5496272fb24
Mario Inga <mario21ic@gmail.com>
2020-04-10 18:11:29 -0500

    feat(session3) fragmentos staticos

---
+++ Session3Fragmentos/app/src/main/java/com/mario21ic/session3fragmentos/EjemplosFragment.java
@@ -0,0 +1,24 @@
+package com.mario21ic.session3fragmentos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class EjemplosFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejemplos_fragment);
+    }
+
+    public void ejempo1(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    public void ejempo2(View view) { startActivity(new Intent(this, ListaCorreo.class));
+
+    }
+}
